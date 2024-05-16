a066ca0a8018aac8d2d0753b3d3e30572c0daa86
Jasmeen Saprah <j.sapraa@gmail.com>
2024-05-16 21:51:07 +0000

    Create flip card effect with css

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+//Variables
+const card = document.getElementsByClassName("inner-flip");
+
+//Event listeners
+card.addEventListener("click", showCard);
+
+/**Hide the thief card randomly*/
+function hideCard(){
+
+}
+
+/**Reveal the card on click*/
+function showCard(){
+card.classList.toggle("flipCard");
+}
+
+/**Check response */
+function checkResponse(){
+
+}
+
+/**Add score */
+function addScore(){
+
+}
