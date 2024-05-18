5e22aa976063042bb40db9d800b59b31420f05f8
Jasmeen Saprah <j.sapraa@gmail.com>
2024-05-18 12:27:24 +0000

    Add event listener for inputs click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,28 @@
 //Variables
 //Get all input properties
-const cardsClicks = document.getElementsByTagName("input");
+const inputs = document.getElementsByTagName("input");
 const cardPlace = document.getElementsByClassName("inner-flip")
 //Get the front, back civilian and werewolf card
 const frontCard = document.getElementsByClassName("front-face");
 const backCard = document.getElementsByClassName("back-face");
 const civilian = document.getElementsByClassName("civilian-front");
-const werewolf = document.getElementById("werewolf-front");
+//const werewolf = document.getElementById("werewolf-front");
+
 //Get the score
 const maxTurns = parseInt(document.getElementById("score-canvas").innerText);
 
 //Event listeners
-document.addEventListener("DomContentLoaded", function () {
-    cardsClicks.addEventListener("click", function () {
-        checkResponse();
-    })
-    runGame();
-})
+document.addEventListener("DOMContentLoaded", function(){
+
+    for(let input of inputs){
+        input.addEventListener("click", function(){
+
+            console.log("click")
+
+        })
+    }
+}
+)
 
 //Run game
 function runGame() {
@@ -42,6 +48,7 @@ function shuffleCard() {
     let index = myArray.indexOf(1);
     console.log("index of wolf image== ", index);
 
+    return index;
 }
 
 shuffleCard();
@@ -49,12 +56,6 @@ shuffleCard();
 /**Check response */
 function checkResponse() {
 
-    let flipCard = false;
-    let backCard = true;
-
-    if (cardsClicks = true) {
-
-    }
 
 }
 
